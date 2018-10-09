02d48291f6a2f62ef920e23e345f9d34694a1179
Mikko Maja <mikmaj88@gmail.com>
2018-10-09 16:19:41 +0300

    Add word validation

--- app1.py
+++ app1.py
@@ -12,7 +12,14 @@ def translate(word):
         return data[word]
     # Check if there are close matches and return the closest word
     elif len(get_close_matches(word, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
+        choice = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                       get_close_matches(word, data.keys())[0])
+        if choice == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif choice == "N":
+            return "The word does not exist! Please double check it."
+        else:
+            return "Sorry! Could not understand your entry."
     else:
         return "The word does not exist! Please double check it."
 
