7be34034d2328619e416c7e90e6ba1e5fcc0fe22
Mikko Maja <mikmaj88@gmail.com>
2018-10-09 16:28:34 +0300

    Format output

--- app1.py
+++ app1.py
@@ -26,4 +26,10 @@ def translate(word):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
