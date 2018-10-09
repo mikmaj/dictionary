8ffd8e979340c319e337c769f3ab077d390248f1
Mikko Maja <mikmaj88@gmail.com>
2018-10-09 16:10:42 +0300

    Add check for similar words

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import json
+from difflib import get_close_matches
 
 data = json.load(open("D:\Projektit\Python\dictionary\data.json"))
 
@@ -9,6 +10,9 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    # Check if there are close matches and return the closest word
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(word, data.keys())[0]
     else:
         return "The word does not exist! Please double check it."
 
